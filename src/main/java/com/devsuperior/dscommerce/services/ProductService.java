8454076a5f581a0b23ca93412a1e7eb395da8390
Denio Oliveira <denio.1838@gmail.com>
2023-08-30 11:02:33 -0300

    Divisão por camadas

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.intities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+		
+		/* 
+		 * Optional<Product> result = repository.findById(id);
+		Product product = result.get();
+		ProductDTO dto = new ProductDTO(product);
+		return dto;
+		*/
+		
+	}
+}
