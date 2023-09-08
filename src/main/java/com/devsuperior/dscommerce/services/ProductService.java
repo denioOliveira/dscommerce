22bb84a1b7dc3902febfad80c944226474fa00d9
Denio Oliveira <denio.1838@gmail.com>
2023-09-08 15:30:14 -0300

    PUT Product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -38,14 +38,24 @@ public class ProductService {
 	
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
-		
 		Product entity = new Product();
+		copyToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}	
+
+	@Transactional
+	public ProductDTO upDate(Long id, ProductDTO dto) {
+		Product entity = repository.getReferenceById(id);
+		copyToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	private void copyToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
-		
-		entity = repository.save(entity);
-		return new ProductDTO(entity);
 	}
 }
